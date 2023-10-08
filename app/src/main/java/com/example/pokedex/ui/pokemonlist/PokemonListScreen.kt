9d3c167b220ae756dd4717a0a61176306ab81ad7
Cristipi <cristipigm@gmail.com>
2023-10-08 19:33:08 +0200

    improve box list and texts

--- app/src/main/java/com/example/pokedex/ui/pokemonlist/PokemonListScreen.kt
+++ app/src/main/java/com/example/pokedex/ui/pokemonlist/PokemonListScreen.kt
@@ -1,16 +1,22 @@
 package com.example.pokedex.ui.pokemonlist
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -52,14 +58,17 @@ fun PokemonItem(
     pokemonType: List<String>,
     img: String
 ) {
-    Row(modifier = Modifier.padding(vertical = 5.dp, horizontal = 10.dp)) {
+    Row(modifier = Modifier.padding(vertical = 5.dp, horizontal = 10.dp)
+        .background(MaterialTheme.colorScheme.primary,
+            RoundedCornerShape(size = 5.dp)),
+        verticalAlignment = Alignment.CenterVertically) {
         Image(
             painter = rememberAsyncImagePainter(img),
             contentDescription = null,
-            modifier = Modifier.size(128.dp)
+            modifier = Modifier.size(80.dp)
         )
         Column(
-            modifier = Modifier.weight(1f)) {
+            modifier = Modifier.padding(start = 10.dp)) {
             Text(modifier = Modifier.fillMaxWidth(), text = id.toString())
             Text(modifier = Modifier.fillMaxWidth(), text = name)
             Row(modifier = Modifier.fillMaxWidth()) {
